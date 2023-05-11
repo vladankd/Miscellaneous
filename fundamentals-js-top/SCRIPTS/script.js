2784fb5a76743cc9faaef8bef87b2f53c5a1c08c
Vladan Babic <vbabic@resourcepos.com>
2023-05-12 00:32:37 +0200

    Commit all

---
+++ fundamentals-js-top/SCRIPTS/script.js
@@ -0,0 +1,28 @@
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "mango",
+  "pear",
+  "peach",
+  "plum",
+  "asparogue",
+];
+
+// fruits.forEach((fruit, index) => {
+//   let uppercased = fruit.charAt(0).toUpperCase() + fruit.slice(1) + " " + index;
+//   console.log(uppercased);
+// });
+// let sFru = fruits.splice(2, fruits.length - 4, " --*--");
+// console.log(fruits);
+// console.log(sFru);
+
+// for (const fruit of fruits) {
+//   console.log(fruit);
+// }
+
+// const newFruits = fruits.map(function (item) {
+//   return item.length;
+// });
+
+// const filteredFruits = fruits.filter((fruit) => fruit.startsWith("a"));
