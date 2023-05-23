3ba7c9bdb0c078dcb3837080de24d5d31864f2ff
Vladan Babic <vbabic@resourcepos.com>
2023-05-23 17:44:24 +0200

    Add all

--- fundamentals-js-top/SCRIPTS/script.js
+++ fundamentals-js-top/SCRIPTS/script.js
@@ -26,3 +26,46 @@ const fruits = [
 // });
 
 // const filteredFruits = fruits.filter((fruit) => fruit.startsWith("a"));
+
+// ================
+// DOM manipulation
+// ================
+
+const container = document.querySelector(".container");
+const domer = document.querySelector(".domer");
+domer.setAttribute("style", "background-color: white; padding: 1.5em;");
+
+const p = document.createElement("p");
+p.setAttribute("style", "color: red;");
+p.innerText = "Hey I'm red!";
+
+const h3 = document.createElement("h3");
+h3.style.color = "blue";
+h3.innerText = "I'm blue h3!";
+
+domer.appendChild(p);
+domer.appendChild(h3);
+
+const div = document.createElement("div");
+div.style.cssText =
+  "border: 3px dashed black; background-color: pink;  color: black; padding: 1em; margin-top: 1.5em;";
+
+div.setAttribute("class", "pinky");
+
+container.appendChild(div);
+
+const h1 = document.createElement("h1");
+h1.innerText = "I'm in a div";
+const p2 = document.createElement("p");
+p2.innerText = "me too!";
+p2.style.cssText = "text-transform: uppercase;";
+
+div.appendChild(h1);
+div.appendChild(p2);
+
+const btn = document.querySelector(".start-stop-btn");
+
+btn.addEventListener("click", (e) => {
+  e.target.style.cssText =
+    "background-color: crimson; color: white; border: none";
+});
